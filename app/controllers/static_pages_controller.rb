d3b857bcd9594a185e1fea201a5d2804598d0894
mhdtahawi <mhd.tahawi@gmail.com>
2013-10-30 02:57:10 +0200

    static_pages controller created, routes file configured for static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contactus
+  end
+
+  def help
+  end
+
+  def terms
+  end
+
+  def press
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contactus" do
+    get :contactus
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+  test "should get terms" do
+    get :terms
+    assert_response :success
+  end
+
+  test "should get press" do
+    get :press
+    assert_response :success
+  end
+
+end
