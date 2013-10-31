a2eaabeac51489ab7416c6bcca8218523eca95ef
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-10-30 20:53:11 -0400

    Add pry gem, this is a better alternative to the rails console. Also started adding the old styles from the previous version of Ogyam. Finally created some partials to handle the layout

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Ogyam
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # use pry as rails console
+    console do
+      require 'pry'
+      config.console = Pry
+    end 
   end
 end
